2e807d7e692fc33bd18503fb5def413f7f06d7ac
Dann <Lesterdannlopez7@gmail.com>
2026-08-13 21:31:27 +0800

    chore: rebrand project to my-app

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: process.env.NEXT_PUBLIC_SITE_NAME || "business-template",
+  name: process.env.NEXT_PUBLIC_SITE_NAME || "my-app",
   githubUrl: process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/Danncode10",
   description: "The backbone template for your next million-dollar idea.",
   url: process.env.NEXT_PUBLIC_SITE_URL || "https://dannflow.vercel.app",
